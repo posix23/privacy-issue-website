2b8ebb8d7068a4edb1a89150d6f9b70d4636aee3
LukeDors <ldorsett@uw.edu>
2021-12-01 17:50:32 -0600

    Added title and changed App.css

--- alexa-app/src/App.js
+++ alexa-app/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        Amazon and Data Ethics
+        <hr></hr>
       </header>
+      <br>
+      </br>
     </div>
   );
 }
