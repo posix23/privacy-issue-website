a981c067db1883110ea84c3676281f125d6cb95b
Toan Ly <toanly@uw.edu>
2021-12-05 13:11:37 -0800

    add texts and update index

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
+const encrypted = require('text/encrypted.txt');
+const unencrypted = require('text/unencrypted.txt');
+const tos = require('text/tos.txt');
+
 app.get("/encrypted", (req, res) => {
-  
+  res.send(encrypted);
+});
+
+app.get("/unencrypted", (req, res) => {
+  res.send(unencrypted);
+});
+
+app.get("/tos", (req, res) => {
+  res.send(tos);
 });
 
 const PORT = process.env.PORT || 8000;
